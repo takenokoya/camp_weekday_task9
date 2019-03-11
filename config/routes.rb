b439fd7c17363ffe84fbcd71f33dab24f82cd979
takenokoya <takuya1002@gmail.com>
2019-03-11 12:16:40 +0900

    Tasksコントローラ、ビューの作成とルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks/hide' => 'tasks#hide'
+  resources :tasks
+  root 'tasks#index'
 end
